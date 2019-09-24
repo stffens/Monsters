0566cb4ac1f6b3ad0b3831ef17fd4eeb75721405
stffens <jasteffens@poligran.edu.co>
2019-09-24 14:54:44 -0500

    Updates

---
+++ precache-manifest.b649094ea87857bdb8ac355fd1671744.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1e017dd8e43d72a1fccf85622084dd6",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "5441e101b1907715e500",
+    "url": "/monsters/static/css/main.f6cafb08.chunk.css"
+  },
+  {
+    "revision": "ae1362b32ad72d9c4678",
+    "url": "/monsters/static/js/2.b075f21b.chunk.js"
+  },
+  {
+    "revision": "5441e101b1907715e500",
+    "url": "/monsters/static/js/main.332d05a5.chunk.js"
+  },
+  {
+    "revision": "870a3376d410fe1d1a18",
+    "url": "/monsters/static/js/runtime~main.ad98243b.js"
+  }
+]);
